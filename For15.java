4b6bd86e547c88b92413ecc68e9ba18de17d5541
IGA RAMADANA SAHPUTRA <139050327+igaramadana@users.noreply.github.com>
2023-10-10 15:54:03 +0700

    Delete For15.java

--- For15.java
+++
@@ -1,8 +0,0 @@
-
-public class For15 {
-    public static void main(String[] args) {
-      for (int i = 1; i <= 5; i++) {
-        System.out.println("Hasil kuadrat dari " + i + " adalah " + (i * i));
-      }  
-    }
-}
