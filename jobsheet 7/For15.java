7b75c4b5ceaffccc78f975e33e06cda5aafa2629
IGA RAMADANA SAHPUTRA <139050327+igaramadana@users.noreply.github.com>
2023-10-10 15:54:17 +0700

    Add files via upload

---
+++ jobsheet 7/For15.java
@@ -0,0 +1,8 @@
+
+public class For15 {
+    public static void main(String[] args) {
+      for (int i = 1; i <= 5; i++) {
+        System.out.println("Hasil kuadrat dari " + i + " adalah " + (i * i));
+      }  
+    }
+}
